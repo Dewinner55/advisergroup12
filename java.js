50557f9fdd012cd7cd4a493e5169cd4cc0b88199
Valentin <evelbrus2@gmail.com>
2023-06-08 07:49:49 +0600

    master54

---
+++ java.js
@@ -0,0 +1,45 @@
+$(function() {
+  var $clientslider = $('#clientlogo');
+  var clients = $clientslider.children().length;
+  var clientwidth = (clients * 220);
+  $clientslider.css('width', clientwidth);
+  var rotating = true;
+  var clientspeed = 1800;
+  var seeclients = setInterval(rotateClients, clientspeed);
+  $(document).on({
+    mouseenter: function() {
+      rotating = false;
+    },
+    mouseleave: function() {
+      rotating = true;
+    }
+  }, '#ourclients');
+  function rotateClients() {
+    if (rotating != false) {
+      var $first = $('#clientlogo li:first');
+      $first.animate({
+        'margin-left': '-220px'
+      }, 2000, function() {
+        $first.remove().css({
+          'margin-left': '0px'
+        });
+        $('#clientlogo li:last').after($first);
+      });
+    }
+  }
+});
+
+new Splide('#splide01', {
+  type: 'fade',
+  autoplay: true, // Установите значение true
+  rewind: true,
+  rewindByDrag: true
+}).mount();
+
+function showImage(element) {
+  element.querySelector('.hover-image').style.display = 'block';
+}
+
+function hideImage(element) {
+  element.querySelector('.hover-image').style.display = 'none';
+}
